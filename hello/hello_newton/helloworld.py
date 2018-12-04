d29c0432b0c7170b45084ba66b5a6a609341d2df
yifuda <xin_soft@foxmail.com>
2018-12-04 22:40:50 +0800

    Hello convex optimization.

--- hello/hello_newton/helloworld.py
+++ hello/hello_newton/helloworld.py
@@ -20,8 +20,21 @@ def sqrt(a, iters=4):
     return x
 
 
+def _cubic(x, a, b, c):
+    return (3 * a * x * x - b * x - c) / (6 * a * x + b)
+
+
+def convex(a, b, c, iters=4, x=0):
+    for _ in range(iters):
+        x = _cubic(x, a, b, c)
+    return x
+
+
 if __name__ == '__main__':
     for a in range(1, 100):
         diff = math.sqrt(a) - sqrt(a, 7)
         assert abs(diff) < 0.0000001, a
+    a, b, c = 1, 3, -6
+    x = convex(a, b, c, iters=20)
+    assert abs(3 * a * x ** 2 + 2 * b * x + c) < 0.0000001, x
     print("Hello Newton's method!")
